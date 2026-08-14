d1098c25fac484df9e04d128fc0675e0e85d8bb8
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-08-14 20:30:58 +0530

    Creating sibling element or using array

--- Day2/script.js
+++ Day2/script.js
@@ -1,5 +1,8 @@
-const h1 =  React.createElement('h1', null, "Hello from React");
+const h1 =  React.createElement('h1', null, "This is h1 tag");
+const h2 =  React.createElement('h2', null, "This is h2 tag");
+
+const div = React.createElement('div', {id: "parent"}, [h1,h2]);
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-root.render(h1)
+root.render(div)
