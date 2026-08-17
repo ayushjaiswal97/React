8e645174550e7162015e78028e08010d800833aa
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-08-17 13:36:11 +0530

    Learn tailwind CSS

---
+++ Day5/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text'>Hello kya hal chal?</h1>
+    </div>
+  )
+}
+
+export default App
