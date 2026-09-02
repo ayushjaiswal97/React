ed2d9364ddd27949fe82a38a3c153a1c84be8929
Ayush Jaiswal <ayushjaiswal9099@gmail.com>
2026-09-02 19:08:30 +0530

    More on useState

--- Day8/src/App.jsx
+++ Day8/src/App.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const arr = ["Ayush", "Akash", "Manish", "Bikash"];
 
-  const [user, setUser] = useState(0);
+  const [marks, setMarks] = useState([93, 81, 95, 72, 33])
+
+  function graceStudent() {
+    const newMarks = marks.map(function(ele){
+      return ele+5
+    })
+    setMarks(newMarks)
+   }
+
   return (
     <div>
-      <h1>{arr[user]}</h1>
-      <button
-        onClick={() => {
-          if (user < arr.length - 1) setUser(user + 1);
-        }}>
-        Change User
-      </button>
+      {marks.map((elem, idx) => {
+        return (
+          <h1 key={idx}>
+            Student {idx + 1} = {elem}
+            ({elem>33? 'Pass' : 'Fail'})
+          </h1>
+        );
+      })}
+
+      <button onClick={graceStudent}>Give them grace</button>
     </div>
   );
 };
