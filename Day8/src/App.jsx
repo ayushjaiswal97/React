21370f40bc02514f213dfb1157de49497dae5f4f
Ayush Jaiswal <ayushjaiswal9099@gmail.com>
2026-09-02 16:52:46 +0530

    Generate random number using useState

--- Day8/src/App.jsx
+++ Day8/src/App.jsx
@@ -1,31 +1,20 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0);
+
+  const [num, setNum] = useState(0)
 
   return (
     <div>
-      <h1>{num}</h1>
-      <button
-        onClick={() => {
-          setNum(num + 1);
-        }}>
-        Increase
-      </button>
-      <button
-        onClick={() => {
-          setNum(num - 1);
-        }}>
-        Decrease
-      </button>
-      <button
-        onClick={() => {
-          setNum(num + 5);
-        }}>
-        Jump by 5
-      </button>
+      <div className="box">
+        {num}
+      </div>
+      <button onClick={()=>{
+        const rdm = Math.floor(Math.random()*100);
+        setNum(rdm);
+      }}>Click here</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
