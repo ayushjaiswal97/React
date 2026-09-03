a900e2214049b7d2cd79a96fc2a428d759d573f8
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-09-04 00:37:10 +0530

    two way binding and showing form details

--- Day9/src/App.jsx
+++ Day9/src/App.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [username, setUsername] = useState("");
 
-  function submitHandler(e){
+  const [allUsers, setallUsers] = useState([]);
+
+  function submitHandler(e) {
     e.preventDefault();
-    console.log('Form Submitted');
+
+    const newUsers = [...allUsers];
+    newUsers.push(username);
+    setallUsers(newUsers);
+
+    setUsername("");
   }
 
   return (
     <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e)
-      }}>
-        <input 
-        type="text" 
-        placeholder='Enter your name'
-        onChange={(e)=>{
-          console.log(e.target.value)
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
         }}
-        value='harsh'
+      >
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={username}
+          required
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
         />
         <button>Submit</button>
       </form>
+      {allUsers.map((elem, idx) => {
+        return <h4 key={idx}>{elem}</h4>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
